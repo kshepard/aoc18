bc1fff5a0cb8a177cbd0d677e9878c550cee0557
Kenny Shepard <kshepard@gmail.com>
2018-12-30 03:26:31 -0500

    Add day 22 part 2

--- src/Day22/Main.hs
+++ src/Day22/Main.hs
@@ -8,9 +8,15 @@ module Main
 where
 
 import           Data.Foldable                  ( foldl' )
+import qualified Data.List                     as L
+import qualified Data.List.Key                 as K
 import qualified Data.Map.Strict               as M
 import           Data.Map.Strict                ( Map )
-import           Data.Maybe                     ( fromMaybe )
+import           Data.Maybe                     ( catMaybes
+                                                , fromMaybe
+                                                )
+import qualified Data.Set                      as S
+import           Data.Set                       ( Set )
 import           Data.Monoid                    ( Sum(..)
                                                 , getSum
                                                 )
@@ -31,10 +37,13 @@ type Parser = Parsec Void String
 data Coord =
   Coord { cx :: !Int
         , cy :: !Int
-        } deriving (Eq, Show)
+        } deriving (Eq)
 
 instance Ord Coord where
   (Coord x1 y1) `compare` (Coord x2 y2) = (y1, x1) `compare` (y2, x2)
+
+instance Show Coord where
+  show Coord {..} = "(" ++ show cx ++ "," ++ show cy ++ ")"
 
 data CaveParams =
   CaveParams { depth  :: !Int
@@ -50,7 +59,7 @@ instance Show RegionType where
   show Narrow = "|"
 
 newtype Cave =
-  Cave (Map Coord RegionType)
+  Cave { unCave :: Map Coord RegionType }
 
 instance Show Cave where
   show (Cave cave) = foldl' f "" cs
@@ -60,6 +69,21 @@ instance Show Cave where
       maxx    = cx maxc
       maxy    = cy maxc
       f acc c = acc ++ show (cave M.! c) ++ (if cx c == maxx then "\n" else "")
+
+data Tool =
+  Torch | ClimbingGear | Neither deriving (Eq, Ord)
+
+instance Show Tool where
+  show Torch        = "T"
+  show ClimbingGear = "C"
+  show Neither      = "N"
+
+data CoordWithTool =
+  CoordWithTool !Coord !Tool deriving (Eq, Ord)
+
+instance Show CoordWithTool where
+  show (CoordWithTool (Coord x y) t) =
+    "(" ++ show x ++ "," ++ show y ++ "," ++ show t ++ ")"
 
 coordParser :: Parser Coord
 coordParser = do
@@ -95,6 +119,81 @@ mkCave CaveParams {..} = Cave $ rt <$> foldl' f M.empty coords
          | otherwise      = el c { cx = cx c - 1 } * el c { cy = cy c - 1 }
     el c = fromMaybe ((gi c + depth) `mod` 20183) $ M.lookup c acc
 
+toolCoords :: Coord -> [CoordWithTool]
+toolCoords c =
+  [CoordWithTool c Torch, CoordWithTool c ClimbingGear, CoordWithTool c Neither]
+
+adjacentCoords :: Coord -> Cave -> [Coord]
+adjacentCoords Coord {..} (Cave cave) = filter
+  (`M.member` cave)
+  [ Coord cx       cy
+  , Coord (cx - 1) cy
+  , Coord (cx + 1) cy
+  , Coord cx       (cy - 1)
+  , Coord cx       (cy + 1)
+  ]
+
+mkPair :: CoordWithTool -> CoordWithTool -> (CoordWithTool, CoordWithTool)
+mkPair ct1@(CoordWithTool c1 _) ct2@(CoordWithTool c2 _)
+  | c1 <= c2  = (ct1, ct2)
+  | otherwise = (ct2, ct1)
+
+toolCoordPairs :: Coord -> Cave -> [(CoordWithTool, CoordWithTool)]
+toolCoordPairs coord cave =
+  [ mkPair c1 c2 | (c1 : cs) <- L.tails coords, c2 <- cs, oneAway c1 c2 ]
+ where
+  coords = adjacentCoords coord cave >>= toolCoords
+  oneAway (CoordWithTool (Coord x1 y1) _) (CoordWithTool (Coord x2 y2) _) =
+    abs (x1 - x2) + abs (y1 - y2) == 1
+
+invalidState :: RegionType -> Tool -> Bool
+invalidState Rocky  Neither      = True
+invalidState Wet    Torch        = True
+invalidState Narrow ClimbingGear = True
+invalidState _      _            = False
+
+mkEdge
+  :: Cave
+  -> (CoordWithTool, CoordWithTool)
+  -> Maybe (CoordWithTool, CoordWithTool, Float)
+mkEdge (Cave cave) (ct1@(CoordWithTool c1 t1), ct2@(CoordWithTool c2 t2))
+  | invalidState rt1 t1 || invalidState rt2 t2 = Nothing
+  | otherwise = Just (ct1, ct2, minutes)
+ where
+  rt1          = cave M.! c1
+  rt2          = cave M.! c2
+  moveMinutes  = if c1 == c2 then 0 else 1
+  equipMinutes = if t1 == t2 then 0 else 7
+  minutes      = moveMinutes + equipMinutes
+
+coordEdges :: Cave -> Coord -> Set (CoordWithTool, CoordWithTool, Float)
+coordEdges cave coord =
+  S.fromList . catMaybes $ mkEdge cave <$> toolCoordPairs coord cave
+
+buildEdges :: Cave -> [(CoordWithTool, CoordWithTool, Float)]
+buildEdges cave = S.toList $ foldl' f S.empty $ M.keys (unCave cave)
+  where f acc c = acc <> coordEdges cave c
+
+buildGraph :: Ord a => [(a, a, Float)] -> Map a [(a, Float)]
+buildGraph g =
+  M.fromListWith (++) $ g >>= \(a, b, d) -> [(a, [(b, d)]), (b, [(a, d)])]
+
+shortestPath :: (Ord a, Show a) => a -> a -> Map a [(a, Float)] -> Float
+shortestPath from to graph = fst $ dijkstra from graph M.! to
+
+dijkstra :: Ord a => a -> Map a [(a, Float)] -> Map a (Float, Maybe a)
+dijkstra source graph = f
+  (M.fromList
+    [ (v, (if v == source then 0 else 1 / 0, Nothing)) | v <- M.keys graph ]
+  )
+  (M.keys graph)
+ where
+  f ds [] = ds
+  f ds q  = f (foldr relax ds $ graph M.! m) (L.delete m q)
+   where
+    m = K.minimum (fst . (ds M.!)) q
+    relax (e, d) = M.adjust (min (fst (ds M.! m) + d, Just m)) e
+
 part1 :: CaveParams -> Cave -> Int
 part1 CaveParams {..} (Cave cave) = getSum $ M.foldMapWithKey f cave
  where
@@ -105,8 +204,16 @@ part1 CaveParams {..} (Cave cave) = getSum $ M.foldMapWithKey f cave
       Wet    -> Sum 1
       Narrow -> Sum 2
 
+part2 :: CaveParams -> Cave -> Int
+part2 CaveParams {..} cave = round $ shortestPath start end graph
+ where
+  graph = buildGraph $ buildEdges cave
+  start = CoordWithTool (Coord 0 0) Torch
+  end   = CoordWithTool target Torch
+
 main :: IO ()
 main = do
   caveParams <- parseCaveParams <$> readFile "input/22.txt"
   let cave = mkCave caveParams
   print $ part1 caveParams cave
+  print $ part2 caveParams cave
